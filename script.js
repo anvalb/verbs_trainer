8a00ba4cc7c84795383a5268df2850aa4216d04a
anvalb <anvalb@gmail.com>
2012-08-28 00:21:55 +0400

    sync

--- script.js
+++ script.js
@@ -5,75 +5,250 @@ $(function(){
     var ruPronouns = new Array("я" ,"ты" ,"мы" ,"они" ,"он" ,"она"),
         enPronouns = new Array("I" ,"you" ,"we" ,"they" ,"he" ,"she"),
         verbs = new Array(
-            [true, "видел", "видела", "вижу", "буду видеть", "видишь", "будешь видеть", "видели", "видим", "будем видеть", "видят", "будут видеть", "видит", "будет видеть", "see", "saw", "seen"]
-            ,[false, "знал", "знала", "знаю", "буду знать", "знаешь", "будешь знать", "знали", "знаем", "будем знать", "знают", "будут знать", "знает", "будет знать", "know"]
+            [true, "видел", "видела", "вижу", "буду видеть", "видишь", "будешь видеть", "видели", "видим",
+                "будем видеть", "видят", "будут видеть", "видит", "будет видеть", "see", "saw", "seen"]
+            ,[false, "знал", "знала", "знаю", "буду знать", "знаешь", "будешь знать", "знали", "знаем", "будем знать",
+                "знают", "будут знать", "знает", "будет знать", "know"]
         ),
         verbsL = verbs.length,
+
     // FUNCTIONS
+
         simpleGenerator = function () {
-            var ruPronounsNow = ruPronouns[Math.floor(Math.random() * 5)],
+            var pronounsNow = Math.floor(Math.random() * 6),
+                ruPronounsNow = ruPronouns[pronounsNow],
+                enPronounsNow = enPronouns[pronounsNow],
                 ruPhrase,
-                verbsNow,
+                enPhrase,
+                ruVerbsNow,
                 i = verbs[Math.floor(Math.random() * (verbsL))],
                 tempArray,
-                formPhrase;
-
-            switch (ruPronounsNow) {
-                case ruPronouns[0]:
-                    tempArray = [i[1], i[2], i[3], i[4]]; // i.slice(1, 4);
-                    verbsNow = tempArray[Math.floor(Math.random() * tempArray.length)];
+                formPhrase = Math.floor(Math.random()*3), // values: 0,1,2
+                verbsTime,
+                enVerbsTime,
+                enVerbsNow;
+            switch (pronounsNow) {
+                case 0:
+                    tempArray = [i[1], i[2], i[3], i[4]];
+                    verbsTime = Math.floor(Math.random() * tempArray.length);
+                    ruVerbsNow = tempArray[verbsTime];
+                    if(verbsTime == 0 || verbsTime == 1){enVerbsTime = "past"}
+                    else {
+                        if (verbsTime == 2){
+                            enVerbsTime = "present"
+                        }
+                        else {
+                            enVerbsTime = "future"
+                        }
+                    }
                     break;
-                case ruPronouns[1]:
-                    tempArray = new Array(i[1], i[2], i[5], i[6]);
-                    verbsNow = tempArray[Math.floor(Math.random() * tempArray.length)];
+                case 1:
+                    tempArray = [i[1], i[2], i[5], i[6]];
+                    verbsTime = Math.floor(Math.random() * tempArray.length);
+                    ruVerbsNow = tempArray[verbsTime];
+                    if(verbsTime == 0 || verbsTime == 1){enVerbsTime = "past"}
+                    else {
+                        if (verbsTime == 2){
+                            enVerbsTime = "present"
+                        }
+                        else {
+                            enVerbsTime = "future"
+                        }
+                    }
                     break;
-                case ruPronouns[2]:
-                    tempArray = new Array(i[7], i[8], i[9]);
-                    verbsNow = tempArray[Math.floor(Math.random() * tempArray.length)];
+                case 2:
+                    tempArray = [i[7], i[8], i[9]];
+                    verbsTime = Math.floor(Math.random() * tempArray.length);
+                    ruVerbsNow = tempArray[verbsTime];
+                    if(verbsTime == 0){enVerbsTime = "past"}
+                    else {
+                        if (verbsTime == 1){
+                            enVerbsTime = "present"
+                        }
+                        else {
+                            enVerbsTime = "future"
+                        }
+                    }
                     break;
-                case ruPronouns[3]:
-                    tempArray = new Array(i[7], i[10], i[11]);
-                    verbsNow = tempArray[Math.floor(Math.random() * tempArray.length)];
+                case 3:
+                    tempArray = [i[7], i[10], i[11]];
+                    verbsTime = Math.floor(Math.random() * tempArray.length);
+                    ruVerbsNow = tempArray[verbsTime];
+                    if(verbsTime == 0){enVerbsTime = "past"}
+                    else {
+                        if (verbsTime == 1){
+                            enVerbsTime = "present"
+                        }
+                        else {
+                            enVerbsTime = "future"
+                        }
+                    }
                     break;
-                case ruPronouns[4]:
-                    tempArray = new Array(i[1], i[12], i[13]);
-                    verbsNow = tempArray[Math.floor(Math.random() * tempArray.length)];
+                case 4:
+                    tempArray = [i[1], i[12], i[13]];
+                    verbsTime = Math.floor(Math.random() * tempArray.length);
+                    ruVerbsNow = tempArray[verbsTime];
+                    if(verbsTime == 0){enVerbsTime = "past"}
+                    else {
+                        if (verbsTime == 1){
+                            enVerbsTime = "present"
+                        }
+                        else {
+                            enVerbsTime = "future"
+                        }
+                    }
                     break;
-                case ruPronouns[5]:
-                    tempArray = new Array(i[2], i[12], i[13]);
-                    verbsNow = tempArray[Math.floor(Math.random() * tempArray.length)];
+                case 5:
+                    tempArray = [i[2], i[12], i[13]];
+                    verbsTime = Math.floor(Math.random() * tempArray.length);
+                    ruVerbsNow = tempArray[verbsTime];
+                    if(verbsTime == 0){enVerbsTime = "past"}
+                    else {
+                        if (verbsTime == 1){
+                            enVerbsTime = "present"
+                        }
+                        else {
+                            enVerbsTime = "future"
+                        }
+                    }
                     break;
             }
-            formPhrase = Math.floor(Math.random()*3);
-            console.log(formPhrase);
+
+            // ending of english verb
+            if(formPhrase == 0 && enVerbsTime == "past"){
+                if (i[0] == true){
+                    enVerbsNow = i[15];
+                }
+                else{
+                    enVerbsNow = i[14] + "ed";
+                }
+            }
+            else {
+                if (formPhrase == 0 && verbsTime == 1 && (pronounsNow == 4 || pronounsNow == 5)) {
+                    enVerbsNow = i[14] + "s";
+                }
+                else {
+                    enVerbsNow = i[14];
+                }
+            }
+
+            //enPhrase
+            enPhrase = "empty";
+            if(enVerbsTime == "past"){
+                switch (formPhrase){
+                    case 0:
+                        enPhrase = enPronounsNow + " " + enVerbsNow + ".";
+                        break;
+                    case 1:
+                        enPhrase = enPronounsNow + " didn't " + enVerbsNow + ".";
+                        break;
+                    case 2:
+                        enPhrase = "Did " + enPronounsNow + " " + enVerbsNow + "?";
+                        break;
+                }
+            }
+            else {
+                if(enVerbsTime == "present"){
+                    switch (formPhrase){
+                        case 0:
+                            enPhrase = enPronounsNow + " " + enVerbsNow + ".";
+                            break;
+                        case 1:
+                            if(enPronounsNow == "he" || enPronounsNow == "she"){
+                                enPhrase = enPronounsNow + " doesn't " + enVerbsNow + ".";
+                            }
+                            else {
+                                enPhrase = enPronounsNow + " don't " + enVerbsNow + ".";
+                            }
+                            break;
+                        case 2:
+                            if(enPronounsNow == "he" || enPronounsNow == "she"){
+                                enPhrase = "Does " + enPronounsNow + " " + enVerbsNow + "?";
+                            }
+                            else {
+                                enPhrase = "Do " + enPronounsNow + " " + enVerbsNow + "?";
+                            }
+                            break;
+                    }
+                }
+                else {
+                    switch (formPhrase){
+                        case 0:
+                            enPhrase = enPronounsNow + " will " + enVerbsNow + ".";
+                            break;
+                        case 1:
+                            enPhrase = enPronounsNow + " will not " + enVerbsNow + ".";
+                            break;
+                        case 2:
+                            enPhrase = "Will " + enPronounsNow + " " + enVerbsNow + "?";
+                            break;
+                    }
+                }
+            }
+            console.log("+++++++++++++++");
+            console.log(enPronounsNow);
+            console.log(ruPronounsNow);
+            console.log(enPhrase);
+            console.log("+++++++++++++++");
+
+            // ruPhrase
             if(formPhrase == 0){
-                ruPhrase = ruPronounsNow + " " + verbsNow + ".";
+                ruPhrase = ruPronounsNow + " " + ruVerbsNow + ".";
             }
             else {
                 if(formPhrase == 1) {
-                    ruPhrase = ruPronounsNow + " " + " не " + verbsNow + ".";
+                    ruPhrase = ruPronounsNow + " " + " не " + ruVerbsNow + ".";
                 }
                 else{
-                    ruPhrase = ruPronounsNow + " " + verbsNow + "?";
+                    ruPhrase = ruPronounsNow + " " + ruVerbsNow + "?";
                 }
             }
+
+
+
+
+
+
+
             ruPhrase = ruPhrase.slice(0, 1).toUpperCase() + ruPhrase.slice(1);
-            return ruPhrase;
+            return {
+                ruPhrase: ruPhrase
+                ,enPhrase: enPhrase
+            };
         },
         parsePhrase = function (){
             return $('.enPhrase').val();
         };
-
+    var simpGen,
+        clicked;
     $("body").on("click", ".startSimple", function(){
-        $('.ruPhrase').html(simpleGenerator());
+        simpGen = simpleGenerator();
+        $('.ruPhrase').html(simpGen['ruPhrase']);
+        $('.outPhraseReal').html(simpGen['enPhrase']);
         $('.outPhrase').hide();
         $('.outPhraseReal').hide();
         $('.enPhrase').val("");
+        $('.ok').removeClass("clicked")
     });
     $("body").on("click", ".ok", function(){
         $('.outPhrase').show().html(parsePhrase());
-        $('.outPhraseReal').show().html("blabla");
-        $('.enPhrase').val("");
+        console.log(simpGen['enPhrase']);
+        console.log(parsePhrase());
+        if(simpGen['enPhrase'] != parsePhrase()){
+            $('.outPhrase').css({
+                'border-color':'#A32000',
+                'background-color':'#FBD8DC',
+                'color':'#AC0400'
+            });
+            console.log('false');
+        }
+        else {
+            $('.outPhrase').css({
+                'border-color':'#00a300',
+                'background-color':'#d9fbe1',
+                'color':'green'
+            });
+        }
+        $('.outPhraseReal').show();
     });
-    $('.ruPhrase').html(simpleGenerator());
 });
