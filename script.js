d820ac858d09d506aab50564ec540513eec21e8d
Smotrov Dmitriy <dsxack@gmail.com>
2012-08-27 20:19:03 +0400

    new Array - это зло используй [] для создания нового массива. так же в твоем случае лучше юзать slice

--- script.js
+++ script.js
@@ -14,27 +14,30 @@ $(function(){
 
             switch (ruPronounsNow){
                 case "я":
-                    tempArray = new Array(i[0], i[1], i[2], i[3]);
+                    // [i[0], i[1], i[2], i[3]]
+                    // также можно заменить на i.slice(0, 3);
+                    // http://javascript.ru/Array/slice
+                    tempArray = [i[0], i[1], i[2], i[3]];
                     verbsNow = tempArray[Math.floor(Math.random()*tempArray.length)];
                     break;
                 case "ты":
-                    tempArray = new Array(i[0], i[1], i[4], i[5]);
+                    tempArray = [i[0], i[1], i[4], i[5]];
                     verbsNow = tempArray[Math.floor(Math.random()*tempArray.length)];
                     break;
                 case "мы":
-                    tempArray = new Array(i[6], i[7], i[8]);
+                    tempArray = [i[6], i[7], i[8]];
                     verbsNow = tempArray[Math.floor(Math.random()*tempArray.length)];
                     break;
                 case "они":
-                    tempArray = new Array(i[6], i[9], i[10]);
+                    tempArray = [i[6], i[9], i[10]];
                     verbsNow = tempArray[Math.floor(Math.random()*tempArray.length)];
                     break;
                 case "он":
-                    tempArray = new Array(i[0], i[11], i[12]);
+                    tempArray = [i[0], i[11], i[12]];
                     verbsNow = tempArray[Math.floor(Math.random()*tempArray.length)];
                     break;
                 case "она":
-                    tempArray = new Array(i[1], i[11], i[12]);
+                    tempArray = [i[1], i[11], i[12]];
                     verbsNow = tempArray[Math.floor(Math.random()*tempArray.length)];
                     break;
             }
